8bbe68352f2871ee63b9d668a93050b5a5cabde5
Do-oya <qkrwjdgus1441@naver.com>
2024-10-04 13:13:48 +0900

    [김영한 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술]

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,11 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mapping/users")
+public class MappingClassController {
+
+
+}
